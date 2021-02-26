3208c36443bab04a67f3411e753a87e4a1ecb5c5
carlita98 <carlam13@ucm.es>
2021-02-26 20:04:08 +0100

    Process signs

--- Filtrado.py
+++ Filtrado.py
@@ -35,9 +35,10 @@ There are 4 levels of kindship so we create 4 columns, each of which contains
 '''
 def process_kindship(df_aux):
     # Create 4 extra columns and fill them with 0s
-    df_aux = df_aux.reindex (columns = df_aux.columns.tolist() +
-                                ['1º grado','2º grado','3º grado', '4º grado'])
-    df_aux.loc[:, ['1º grado','2º grado','3º grado', '4º grado']] = 0
+    new_columns = ['1º grado','2º grado','3º grado', '4º grado']
+    for column in new_columns:
+        if (column not in df_aux.columns):
+            df_aux.insert(0, column, 0)
 
     # In case there is a i-level of kindship it fills the column 'iº grado' with '1'
     for i in range(1, 4):
@@ -45,7 +46,7 @@ def process_kindship(df_aux):
             df_aux['Grado de parentesco (si hay más de 1)'].str.contains(str(i)), str(i)+'º grado'] = 1
 
     # Delete the previous columns releated to kindship
-    df_aux.drop(columns=['Grado de parentesco', 'Grado de parentesco (si hay más de 1)'])
+    df_aux = df_aux.drop(columns=['Grado de parentesco', 'Grado de parentesco (si hay más de 1)'])
     return df_aux
 
 '''
@@ -53,16 +54,14 @@ For each different value in 'previous_columns' we create a column. Each record c
 the row has not that value and 1 in case the row has that value
 '''
 def process_columns_to_binary(df_aux, delete_more, records_number, previous_columns):
-
-    # create an array containing the values in previous_colums 
+    # Create an array containing the values in previous_colums 
     new_columns= pd.unique(df_aux[previous_columns].values.ravel('K'))
     # Filter out nan value and delete_more in array
     new_columns = list(filter(lambda i: not i in [np.nan] + delete_more, new_columns))
 
-    # Add a new column per desease
-    df_aux = df_aux.reindex (columns = df_aux.columns.tolist() +
-                                new_columns)
-    df_aux.loc[:, new_columns] = 0
+    for column in new_columns:
+        if (column not in df_aux.columns):
+            df_aux.insert(0, column, 0)
 
     for i in range(0, len(new_columns)):
         for j in range(records_number):
@@ -72,18 +71,18 @@ def process_columns_to_binary(df_aux, delete_more, records_number, previous_colu
                     df_aux.loc[:,new_columns[i]].iloc[j] = 1
     
     # Delete previous columns 
-    df_aux.drop(columns=previous_columns)
+    df_aux = df_aux.drop(columns=previous_columns)
     return df_aux
 
 
 def main():
     df = read_data_from_local()
-    df.excel("DatosBonitos.xlsx")
     df_aux = df
     records_number = df.iloc[:,0].size
     df_aux = process_kindship(df_aux)
     df_aux = process_columns_to_binary(df_aux,to_delete["immunological_desease"], records_number, column_names["immunological_desease"])
     df_aux = process_columns_to_binary(df_aux,to_delete["symptoms"], records_number, column_names["symptoms"])
+    df_aux = process_columns_to_binary(df_aux,to_delete["signs"], records_number, column_names["signs"])
     df_aux.to_excel("filterData.xlsx")
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -1,10 +1,12 @@
 column_names = {
   "immunological_desease": ['Enfermedad inmunológica', 'Enfermedad inmunológica (si hay más de 1)', 
                 'Enfermedad inmunológica (si hay más de 2)'],
-  "symptoms": ['Síntomas específicos', 'Síntomas específicos.1', 'Síntomas específicos.2', 'Otros síntomas']
+  "symptoms": ['Síntomas específicos', 'Síntomas específicos.1', 'Síntomas específicos.2', 'Otros síntomas'],
+  "signs": ['Signos  ', 'Signos 2  ', '  Signos 3']
 }
 
 to_delete = {
     "immunological_desease": ["Otra"], 
-    "symptoms": ["Otros (especificar en otros síntomas)"]
+    "symptoms": ["Otros (especificar en otros síntomas)"],
+    "signs": ["Nada"]
 }

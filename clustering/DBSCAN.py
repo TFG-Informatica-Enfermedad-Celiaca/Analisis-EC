1e88ee6471a9cc43d1928faaa4597c98f818f83c
Pablo <pablo@MacBook-Pro-de-Pablo.local>
2021-03-30 22:12:35 +0200

    DBSCAN Algorithm

---
+++ clustering/DBSCAN.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 26 11:58:12 2021
+
+@author: pablo
+@author: Carla
+"""
+
+import pandas as pd
+import numpy as np
+import plotly.io as pio
+pio.renderers.default='browser'
+import sys
+sys.path.append(r'../')
+from loadData import read_numerical_data_from_local
+from reduceDimension import reduce_dimension_global_data_plotly, reduce_dimension_after_clustering
+from scoreF1 import f1_score
+from sklearn.cluster import DBSCAN
+from sklearn.decomposition import PCA
+from matplotlib import pyplot as plt
+import sklearn as sklearn
+from sklearn.neighbors import kneighbors_graph
+
+
+
+def main ():
+    
+    reduce_dimension_global_data_plotly()
+    full_data = read_numerical_data_from_local()
+    data = full_data.drop(columns = ['Diagnóstico'])
+    
+    ## Parametrización de DBSCAN.
+    estimator = PCA (n_components = 2)
+    X_pca = estimator.fit_transform(data)
+    dist = sklearn.neighbors.DistanceMetric.get_metric('euclidean')
+    matsim = dist.pairwise(X_pca)
+    minPts  = 2 # Fijamos el parámetro minPts
+    A = kneighbors_graph(X_pca, minPts, include_self=False)
+    Ar = A.toarray()
+    seq = []
+    for i,s in enumerate(X_pca):
+        for j in range(len(X_pca)):
+            if Ar[i][j] != 0:
+                seq.append(matsim[i][j])
+    seq.sort()
+    plt.plot(seq)
+    plt.show()
+    
+
+    db = DBSCAN(eps=0.7, min_samples=2, algorithm='auto').fit(data)
+    clusters = db.fit_predict(data)
+    aux = len(db.core_sample_indices_)
+    
+    print(aux)
+    
+    reduce_dimension_after_clustering(clusters, aux)
+                                
+    f1_score(clusters)
+    
+    
+    copy = pd.DataFrame()
+    copy['label'] = clusters;
+    cantidadGrupo =  pd.DataFrame()
+    cantidadGrupo['cantidad']=copy.groupby('label').size()
+    print(cantidadGrupo)
+    
+    
+    
+    
+    
+    
+if __name__ == "__main__":
+    main()
+    
---
+++ clustering/agglomerative.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 26 11:58:12 2021
+
+@author: pablo
+@author: Carla
+"""
+
+import pandas as pd
+import numpy as np
+import plotly.io as pio
+pio.renderers.default='browser'
+import sys
+sys.path.append(r'../')
+from loadData import read_numerical_data_from_local
+from reduceDimension import reduce_dimension_global_data_plotly, reduce_dimension_after_clustering
+from scoreF1 import f1_score
+from sklearn.cluster import AgglomerativeClustering
+from scipy.cluster.hierarchy import dendrogram
+from matplotlib import pyplot as plt
+
+
+
+
+
+def plot_dendrogram(model, **kwargs):
+    # Create linkage matrix and then plot the dendrogram
+
+    # create the counts of samples under each node
+    counts = np.zeros(model.children_.shape[0])
+    n_samples = len(model.labels_)
+    for i, merge in enumerate(model.children_):
+        current_count = 0
+        for child_idx in merge:
+            if child_idx < n_samples:
+                current_count += 1  # leaf node
+            else:
+                current_count += counts[child_idx - n_samples]
+        counts[i] = current_count
+
+    linkage_matrix = np.column_stack([model.children_, model.distances_,
+                                      counts]).astype(float)
+
+    # Plot the corresponding dendrogram
+    dendrogram(linkage_matrix, **kwargs)
+
+    
+
+
+
+
+def main ():
+    reduce_dimension_global_data_plotly()
+    full_data = read_numerical_data_from_local()
+    data = full_data.drop(columns = ['Diagnóstico'])
+    
+    number_clusters = 6;
+
+    for metric in ['ward', 'complete', 'average', 'single']:
+        model = AgglomerativeClustering(
+            n_clusters = number_clusters, affinity='euclidean', memory = None, connectivity = None,
+            compute_full_tree='auto', linkage = metric, distance_threshold = None,
+            compute_distances=True)
+        
+        clusters = model.fit_predict(data)
+        reduce_dimension_after_clustering(clusters, number_clusters)
+        f1_score(clusters)
+        plot_dendrogram(model, truncate_mode='level', p=number_clusters)
+        plt.title(metric)
+        plt.xlabel("Number of points in node (or index of point if no parenthesis).")
+        plt.show()
+    
+if __name__ == "__main__":
+    main()
+    
